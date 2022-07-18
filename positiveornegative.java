79a3a413905597fa8bf74868fdfa7c5bed610ed1
Akhil <akhilmallela83@gmail.com>
2022-07-18 12:58:37 +0530

    add positiveornegative.java

---
+++ positiveornegative.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class positiveornegative {
+
+	public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	System.out.println("Input number:");
+	int input =35;
+	 
+	if (35> 0);
+	{
+		System.out.println("number is positive ");
+	}
+	
+	
+	
+			
+			
+				
+	}
+
+}
+;
